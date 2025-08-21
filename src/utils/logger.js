7b056ed1671d1279c27cdb13fc2039374f2d1e63
SrAquaGenius <dimoisivic@gmail.com>
2025-08-21 16:28:16 +0100

    fix: dividir ficheiro logger.js em logger.js e debug.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,58 +3,6 @@
  * Authors:  SrAqua
  * ------------------------------------------------------------------------- */
 
-let debugFlag = false;
-let debugPlusFlag = false;
-
-/** @brief troca o estado de debug atual */
-function mudarDebug() {
-	debugFlag = !debugFlag;
-	log(`🐞 Modo de debug ${debugFlag ? "ativado" : "desativado"}`);
-}
-
-/** @brief Devolve o estado de debug atual */
-function mostrarDebug() {
-	return debugFlag;
-}
-
-/**
- * Imprime mensagens de debug, indicando a função de chamada.
- * @param {...any} mensagens Mensagens a imprimir em modo debug.
- */
-function debug(...mensagens) {
-
-	if (debugFlag) {
-		const stack = new Error().stack;
-		const linhas = stack.split("\n");
-		let origem = "desconhecida";
-
-		// Tenta obter a função chamadora
-		if (linhas.length >= 3) {
-			const linha = linhas[2];
-
-			// Caso com nome da função: at separarSilabas (/caminho/ficheiro.js:linha:col)
-			let matchFuncao = linha.match(/at (\S+) \(([^)]+)\)/);
-
-			if (matchFuncao) {
-				origem = matchFuncao[1];
-			}
-			else {
-				// Caso sem nome da função: at /caminho/ficheiro.js:linha:col
-				let matchFicheiro = linha.match(/at .*\/([^\/:]+):\d+:\d+/);
-				if (matchFicheiro) {
-					origem = matchFicheiro[1];
-				}
-			}
-		}
-
-		console.log(`[DEBUG][${origem}]`, ...mensagens);
-	}
-}
-
-function debugPlus(...mensagens) {
-	if (debugPlusFlag) debug(...mensagens);
-}
-
 function log(...args) {
 	console.log(...args);
 }
@@ -76,5 +24,4 @@ function clear() {
 }
 
 
-module.exports = { mostrarDebug, mudarDebug, debug, debugPlus,
-				   log, warn, error, todo, clear };
+module.exports = { log, warn, error, todo, clear };
