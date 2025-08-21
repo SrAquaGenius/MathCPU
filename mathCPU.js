9cec1d3fcb67824bc43107e61768973876c69bcd
SrAquaGenius <dimoisivic@gmail.com>
2025-08-21 16:06:16 +0100

    feat: adicionado o ficheiro inicial e as resptivas definições

--- mathCPU.js
+++ mathCPU.js
@@ -3,33 +3,38 @@
  * Authors:  SrAqua
  * ------------------------------------------------------------------------- */
 
-const { log, todo } = require("./src/utils/logger");
+const { clear, log, todo } = require("./src/utils/logger");
 const { criarInterface } = require("./src/utils/input");
 
 const { menuAlgebra } = require("./src/menu/menuAlgebra");
 
+
+const input = criarInterface();
+menuPrincipal();
+
+
 function menuPrincipal() {
-    log("\n=== MathCPU ===");
-    log("1. Álgebra");
-    log("2. Cálculo");
-    log("3. Geometria");
-    log("4. Teoria dos Números");
-    log("5. Estatística");
-    log("0. Sair");
 
-    const rl = criarInterface();
+	clear();
+	log("\n=== MathCPU ===");
+	log("1. Álgebra");
+	log("2. Cálculo");
+	log("3. Geometria");
+	log("4. Teoria dos Números");
+	log("5. Estatística");
+	log("0. Sair");
 
-    rl.question("Escolha uma opção: ", (resposta) => {
-        switch (resposta) {
-            case "1": menuAlgebra(); break;
-            case "2": todo(); break;
-            case "3": todo(); break;
-            case "4": todo(); break;
-            case "5": todo(); break;
-            case "0": rl.close(); break;
-            default: log("❌ Opção inválida"); menuPrincipal();
-        }
-    });
+	input.question("Escolha uma opção: ", (resposta) => {
+		switch (resposta) {
+			case "1": menuAlgebra(input, menuPrincipal); break;
+			case "2": todo(); break;
+			case "3": todo(); break;
+			case "4": todo(); break;
+			case "5": todo(); break;
+			case "0": input.close(); break;
+			default: log("❌ Opção inválida"); menuPrincipal();
+		}
+	});
 }
 
 module.exports = { menuPrincipal };
